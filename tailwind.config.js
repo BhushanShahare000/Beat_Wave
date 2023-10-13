e5feaf4f3b67da80c3c02352bf1f55cd689fa986
BhushanShahare000 <140686587+BhushanShahare000@users.noreply.github.com>
2023-10-13 10:48:51 +0530

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  'watch': 'npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch',
+  theme: {
+    extend: {
+
+      screens:{
+        'sm':'480px',
+
+
+      },
+      fontFamily:{
+        'Autour+One':['Autour+One'],
+        'Montserrat':['Montserrat'],
+      },
+
+      boxShadow: {
+        'shw': '0 15px 20px  pink',
+      },
+   
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        animation: {
+          wiggle: 'wiggle 1s ease-in-out infinite',
+        },
+      },
+
+
+      },
+  },
+  plugins: [],
+}
+
+
